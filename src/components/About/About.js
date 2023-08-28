25c3c043ca85624cfe767ac3834f56469ed588ce
joynalmdabedin <joynalmdabedin440@gmail.com>
2023-08-28 21:28:04 +0600

    about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,30 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>Secret about us!!!</h2>
+            <h2> Secret about us!!!</h2>
+            <p>Convenience: One of the major benefits of an e-commerce site is the convenience it provides to shoppers. Customers can browse through your products, compare prices, and make purchases from the comfort of their homes or on-the-go using their devices. This eliminates the need to travel to physical stores.
+
+                24/7 Availability: Unlike brick-and-mortar stores with specific operating hours, your e-commerce site is accessible around the clock. This allows customers to shop whenever it's convenient for them, regardless of time zones or busy schedules.
+
+                Wide Product Range: E-commerce sites often have the ability to offer a broader range of products compared to traditional stores due to not being limited by physical shelf space. This variety attracts different types of customers and caters to diverse needs.
+
+                Detailed Product Information: E-commerce sites can provide detailed product descriptions, specifications, images, and sometimes even videos, which assist customers in making informed purchasing decisions. This reduces uncertainty and enhances the shopping experience.
+
+                Search and Filter Options: Effective search and filter functionalities help customers quickly find the products they're looking for. This enhances user experience by saving time and reducing frustration.
+
+                Customer Reviews and Ratings: Positive reviews and ratings from previous customers can build trust and credibility. They also offer insights to potential buyers about the quality and usability of products.
+
+                Personalization: E-commerce sites can use customer data to provide personalized recommendations based on past purchases and browsing behavior. This creates a tailored shopping experience that often leads to higher customer satisfaction and increased sales.
+
+                Global Reach: With an e-commerce site, your business can reach a global audience without the need for physical stores in multiple locations. This opens up opportunities for international expansion.
+
+                Cost Savings: Running an online store typically involves lower overhead costs compared to maintaining physical stores. You can save on expenses related to rent, utilities, and in-store personnel.
+
+                Easy Payment and Checkout: E-commerce sites offer various secure payment options, making the checkout process seamless and hassle-free for customers. This reduces cart abandonment rates.
+
+                Analytics and Insights: E-commerce platforms often provide data analytics that offer insights into customer behavior, popular products, conversion rates, and more. This information can help you refine your strategies and improve your business.
+
+                Accessibility: E-commerce sites are accessible to people with disabilities, as they can be designed with accessibility features that cater to a wider audience.</p>
         </div>
     );
 };
--- src/components/shipping/Shipping.js
+++ src/components/shipping/Shipping.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Shipping = () => {
     return (
         <div>
-            <h1>shipping</h1>
+            <h1>Easy shipping</h1>
+            <h2>Your product is on the way to delivery.It perhaps take two days</h2>
         </div>
     );
 };
